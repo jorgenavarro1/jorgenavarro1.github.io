5005127e5aff686c63e702814e8980b925976b47
jorgenavarro1 <jnavarro11@hawk.iit.edu>
2026-04-01 20:38:37 -0500

    Edit layout of tiles

--- Lab4/jnavarro-lab4.js
+++ Lab4/jnavarro-lab4.js
@@ -51,3 +51,16 @@
       icon.textContent = "ads_click";
     }
   });
+
+// 3a. Change the layout of the tiles
+  const style = document.createElement("style");
+  style.textContent = `
+    @media (min-width: 1024px) {
+      #solutions .grid {
+        grid-template-columns: repeat(4, 1fr) !important;
+      }
+    }
+  `;
+  document.head.appendChild(style);
+
+  
