e52309200879f67461c3493e0c98863d62cee9ed
jorgenavarro1 <jnavarro11@hawk.iit.edu>
2026-04-01 19:10:35 -0500

    Edit text below headline to corrected text

--- Lab4/jnavarro-lab4.js
+++ Lab4/jnavarro-lab4.js
@@ -2,7 +2,7 @@
 //ITMD-441/02
 
 (function () {
-  console.log("Lab 4 script running...");
+  console.log("Lab 4 script");
  
   // 1a. Change the main headline text in the hero section
   const heroHeadline = document.querySelector("section h1");
@@ -10,4 +10,9 @@
     heroHeadline.textContent = "Uplift Your Brand with Stellar Marketing";
   }
 
-  
+    // 1b. Edit text below the hero headline
+  const heroSubtext = document.querySelector("section p");
+  if (heroSubtext) {
+    heroSubtext.innerHTML =
+      "<em>Utilize cutting-edge strategies from Stellar Marketing to help your business <strong>thrive</strong> and <strong>excel</strong>.</em>";
+  }
