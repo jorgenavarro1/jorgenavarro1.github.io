92f68b01e1dbe21c0c447a5256f51834b68bea2a
jorgenavarro1 <jnavarro11@hawk.iit.edu>
2026-04-01 19:08:33 -0500

    edit text in hero section

---
+++ Lab4/jnavarro-lab4.js
@@ -0,0 +1,13 @@
+//jnavarro-Lab4
+//ITMD-441/02
+
+(function () {
+  console.log("Lab 4 script running...");
+ 
+  // 1a. Change the main headline text in the hero section
+  const heroHeadline = document.querySelector("section h1");
+  if (heroHeadline) {
+    heroHeadline.textContent = "Uplift Your Brand with Stellar Marketing";
+  }
+
+  
