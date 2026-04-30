be2f1e2e47fc05549e758258db798c32ba7abaaf
jorgenavarro1 <jnavarro11@hawk.iit.edu>
2026-04-29 23:41:44 -0500

    add js for button

--- Lab6/main.js
+++ Lab6/main.js
@@ -4,3 +4,15 @@ const todayCard = document.getElementById("todayCard");
 const tomorrowCard = document.getElementById("tomorrowCard");
 const errorMsg = document.getElementById("errorMsg");
 const timezoneDisplay = document.getElementById("timezoneDisplay");
+
+btn.addEventListener("click", () => {
+  errorMsg.textContent = "";
+
+  const value = select.value;
+
+  if (!value) {
+    errorMsg.textContent = "Please select a location.";
+    return;
+  }
+
+  const [lat, lng] = value.split(",");
