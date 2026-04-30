713685715cf087f83db88c04e4f4f8f7f3a3b03d
jorgenavarro1 <jnavarro11@hawk.iit.edu>
2026-04-29 23:41:11 -0500

    create consts for js

---
+++ Lab6/main.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("getDataBtn");
+const select = document.getElementById("locationSelect");
+const todayCard = document.getElementById("todayCard");
+const tomorrowCard = document.getElementById("tomorrowCard");
+const errorMsg = document.getElementById("errorMsg");
+const timezoneDisplay = document.getElementById("timezoneDisplay");
